5a1b24cb67e8cb9fbbf1f544dc9ac310207efefe
Jon Baird <jon.baird@northstate.net>
2017-03-09 21:46:44 -0500

    .

--- project_euler12.py
+++ project_euler12.py
@@ -1,1 +1,22 @@
 """ Problem #12"""
+
+
+class Euler12(object):
+
+    def tri(self, n=1):
+        """ calculate triangle number """
+        try:
+            assert(n >= 0)
+        except AssertionError:
+            print("please supply an number >= 0")
+            return -1
+        else:
+            return self._tri(n)
+
+    @staticmethod
+    def _tri(n=1):
+        """ return total """
+        tot = 0
+        for x in range(1, n + 1):
+            tot += x
+        return tot
