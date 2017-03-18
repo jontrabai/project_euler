05e012a64a53770e64bfae4c367d17fe0f139567
Jon Baird <jon.baird@northstate.net>
2017-03-18 15:58:35 -0400

    problem 16 complete.

---
+++ project_euler92.py
@@ -0,0 +1,53 @@
+"""
+problem 92
+"""
+
+
+def sum_sqr_dig(n=1):
+    """
+    Add the squares of the digits of a number
+    :param n: an integer
+    :return: the sum of the sqares of the digits of n
+    """
+    nstr = str(n)
+    nl = []
+    result = 0
+    for i in range(len(nstr)):
+        nl.append(int(nstr[i]) ** 2)
+    for i in range(len(nl)):
+        result += nl[i]
+    return result
+
+
+def make_seq(n=1):
+    """
+    combine the squares of the digits of a number into a sequence
+    :param n: the starting number
+    :return: the sequence of numbers
+    """
+    l = [n]
+    idx = 0
+    while sum_sqr_dig(l[idx]) >= 1 or sum_sqr_dig(l[idx]) != 89:
+        if sum_sqr_dig(l[idx]) == 1 or sum_sqr_dig(l[idx]) == 89:
+            l.append(sum_sqr_dig(l[idx]))
+            break
+        else:
+            l.append(sum_sqr_dig(l[idx]))
+            idx += 1
+    return l
+
+
+num_end = 10000000
+count_89 = 0
+count_1 = 0
+for i in range(1, num_end):
+    seq = make_seq(i)
+    if seq[len(seq) - 1] == 1:
+        count_1 += 1
+    elif seq[len(seq) - 1] == 89:
+        count_89 += 1
+    else:
+        raise Exception
+
+print("ends with 1:", count_1)
+print("ends with 89:", count_89)
